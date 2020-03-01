c29127f5f85a3eb07ccc5926b5a8a00bf8cd7529
Shane <shane.boyar@gmail.com>
2020-03-01 10:59:31 -0500

    chore(release): 0.0.1

--- .versionrc.js
+++ .versionrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   bumpFiles: [
-    {
-      filename: 'package.json',
-    },
-    {
-      filename: 'app.json',
-      updater: require.resolve('standard-version-expo'),
-    },
+    // {
+    //   filename: 'package.json',
+    // },
+    // {
+    //   filename: 'app.json',
+    //   updater: require.resolve('standard-version-expo'),
+    // },
     {
       filename: 'app.json',
       updater: require.resolve('standard-version-expo/ios'),
